ac4a1663f49d9aee45f6ea5a3516e85041ebf86e
thisguyisbarry <thisguyisbarry@gmail.com>
2021-03-29 14:35:41 +0100

    Quick mockup of calendar view, needs actual design work done by design team.

--- app/src/main/java/com/TeamBee/WellbeingTracker/CalendarActivity.java
+++ app/src/main/java/com/TeamBee/WellbeingTracker/CalendarActivity.java
@@ -3,6 +3,7 @@ package com.TeamBee.WellbeingTracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+    }
+
+    public void calendar_openHelp(View view) {
+    }
+
+    public void calendar_openHome(View view) {
+    }
+
+    public void calendar_openTracker(View view) {
+    }
+
+    public void calendar_openWellbeing(View view) {
+    }
+
+    public void calendar_openCalendar(View view) {
     }
 }
